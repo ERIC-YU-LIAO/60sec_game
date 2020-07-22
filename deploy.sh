f0994709467024c63cb45948f6f8ca1cfd3b83b3
ERIC-YU-LIAO <ERIC-YU-LIAO@users.noreply.github.com>
2020-07-22 13:34:37 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ git add -A
 git commit -m 'deploy'
 # 部署到 https://github.com/chou0728/eric-project.git 分支為 gh-pages
 git push -f https://github.com/ERIC-YU-LIAO/60sec_game master:gh-pages
-//將dist資料夾中的內容推送至遠端eric-project的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
+# //將dist資料夾中的內容推送至遠端eric-project的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
 cd -
